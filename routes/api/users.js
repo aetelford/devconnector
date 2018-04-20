3dee1abce55e5d164eceea14d4bc048e7a093801
aetelford <35819787+aetelford@users.noreply.github.com>
2018-04-20 15:56:19 -0400

    finishing back-end

--- routes/api/users.js
+++ routes/api/users.js
@@ -89,17 +89,12 @@ router.post('/login', (req, res) => {
         // User matched
         const payload = { id: user.id, name: user.name, avatar: user.avatar }; // create jwt payload
         // Sign token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          { expiresIn: 3600 },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: 'Bearer ' + token
-            });
-          }
-        );
+        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+          res.json({
+            success: true,
+            token: 'Bearer ' + token
+          });
+        });
       } else {
         errors.password = 'Password incorrect';
         return res.status(400).json(errors);
@@ -111,16 +106,12 @@ router.post('/login', (req, res) => {
 // @route   GET api/users/current
 // @desc    Return current user
 // @access  Private
-router.get(
-  '/current',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.json({
-      id: req.user.id,
-      name: req.user.name,
-      email: req.user.email
-    });
-  }
-);
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  });
+});
 
 module.exports = router;
